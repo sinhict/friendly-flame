1b7f22b750d08ef8fc21224ca3f32db74230007c
Thistis@gmail.com <Thistis@gmail.com@b780ceed-e55b-427b-57da-bcf0b255c149>
2013-01-16 14:05:50 +0000

    updated: - new ArduinoServices class, will have Arduino methods later - Flame.class now can recalculate brightness value for outgoingness for arduino - Flame.class has now methods to to generate color codes for arduino, arduino communication still needs to be implemented though - Flame.class has method for converting RFID tags to strings

--- trunk/ friendly-flame/HelloFacebookSample/src/com/facebook/samples/hellofacebook/Flame.java
+++ trunk/ friendly-flame/HelloFacebookSample/src/com/facebook/samples/hellofacebook/Flame.java
@@ -1,7 +1,27 @@
 package com.facebook.samples.hellofacebook;
 
+import android.graphics.Color;
+import android.test.IsolatedContext;
+
 //class to controll the status of the flame
+
+/**
+ * FRIENDLY FLAME COLOR MODES and codes:
+ * 
+ * green (G) = accepted
+ * red (R) = declined
+ * orange (O) = maybe (mixed from yellow and red LED)
+ * yellow (Y) = outgoingness (brightness is changing depending on outgoingness)
+ * 
+ * @author Kahochan
+ *
+ */
 public class Flame {
+	
+	//blue chip, red chip and white rfid tags
+	String acceptRSP = "01005E0D7010";
+    String declineRSP = "3C00CEB1F3C0";
+    String maybeRSP = "0300B12C7030";
 	
 	//to calculate the outgoingness of a user
     public float[] calculateOutgoingness() {
@@ -18,13 +38,69 @@ public class Flame {
     	//currently it's hardcoded, but it should depend
     	//on the value of the events
     	
-    	float outgoingness = 0.8f;
+    	//float outgoingness = 0.8f;
     	//calculate outgoingness by events;
-    	hsv[2] = outgoingness;
+    	// hsv[2] = outgoingness;
+    	hsv[2] = 0.9f;
     	
+    	String outgoingnessColor = changeHSVToString(hsv);
+    	
+    	//send color to Arduino and change login background color
+    	changeColor(outgoingnessColor);
     	return hsv;
-    	
-    	//TODO: WERT an Arduino Ÿbermitteln
     }
+    
+    //method to change HSV color to string with format Y+colorValue 
+    //colorValue lies between 0 and 255
+    public String changeHSVToString(float[] color) {
+    		float brightness = color[2];
+    		double brightnessConverted = 0;
+    		if (brightness == 1) {
+    			brightnessConverted = 255;
+    		}
+    		else {
+    			brightnessConverted = brightness * 256.0;
+    		}
+    			StringBuffer yellowColor = new StringBuffer("Y");
+    			yellowColor.append(brightnessConverted);
+    			return yellowColor.toString();
+    }
+    
+    //method to send color changes to Arduino flame when no brightness is needed
+    public void changeColor(String color) {
+    	
+    	//string format:
+    	//first letter - Color (G, R, Y, O)
+    	//second letter - brightness value (only for Y), e.g. Y128 -> Yellow with Brightness 128
+    	
+    	//TODO call method to send string to Arduino
+    	
+    }
+    
+    //method for processing RFID reader input for decisions
+    //IDs:
+    //blue chip -> accept
+    //red chip -> decline
+    //white card -> maybe
+    
+    public void rspEvent(String rfid) {
+    	
+    	//TODO receive input from Arduino, will be RFID ID tag
+    	String decision = "";
+    	
+    	if (rfid.equals(acceptRSP)) {
+    		decision = "accept";
+    	}
+    	if (rfid.equals(declineRSP)) {
+    		decision = "decline";
+    	}
+    	if (rfid.equals(maybeRSP)) {
+    		decision = "maybe";
+    	}
+    			
+    	//TODO sent decision to FB event
+    }
+    
+    
 
 }
